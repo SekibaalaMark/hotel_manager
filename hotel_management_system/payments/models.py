3dd7e7f5b213c38a64a32d8b481be54cbb9c6dcb
sekibaalamark <markmajje256@gmail.com>
2026-03-09 16:57:21 +0300

    created payment model

--- hotel_management_system/payments/models.py
+++ hotel_management_system/payments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+
+    METHODS = [
+        ("cash", "Cash"),
+        ("card", "Card"),
+        ("mobile_money", "Mobile Money")
+    ]
+
+    booking = models.ForeignKey('bookings.Booking', on_delete=models.CASCADE)
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    payment_method = models.CharField(max_length=20, choices=METHODS)
+
+    paid_at = models.DateTimeField(auto_now_add=True)
+
+    successful = models.BooleanField(default=True)
