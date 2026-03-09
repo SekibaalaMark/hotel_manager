eed0b98f676fb3de4323e3782374f2f68d6e9074
sekibaalamark <markmajje256@gmail.com>
2026-03-09 16:53:58 +0300

    created room model

--- hotel_management_system/rooms/models.py
+++ hotel_management_system/rooms/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+
+    ROOM_TYPES = [
+        ("single", "Single"),
+        ("double", "Double"),
+        ("suite", "Suite")
+    ]
+
+    number = models.CharField(max_length=10, unique=True)
+    room_type = models.CharField(max_length=20, choices=ROOM_TYPES)
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2)
+
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.number
