8ba4b774657bdfdf48b698f2cb4028b814417b72
sekibaalamark <markmajje256@gmail.com>
2026-03-09 16:50:52 +0300

    added the apps to settings

--- hotel_management_system/hotel_management_system/settings.py
+++ hotel_management_system/hotel_management_system/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'accounts',
+    'rooms',
+    'bookings',
+    'payments',
+
 ]
 
 MIDDLEWARE = [
