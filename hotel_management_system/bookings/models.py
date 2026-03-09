fc213af73c451ba085e629998aa5d470b832e44b
sekibaalamark <markmajje256@gmail.com>
2026-03-09 16:55:44 +0300

    created booking model

--- hotel_management_system/bookings/models.py
+++ hotel_management_system/bookings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+
+    STATUS = [
+        ("pending", "Pending"),
+        ("confirmed", "Confirmed"),
+        ("checked_in", "Checked In"),
+        ("checked_out", "Checked Out"),
+        ("cancelled", "Cancelled"),
+    ]
+
+    guest = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    room = models.ForeignKey('rooms.Room', on_delete=models.CASCADE)
+
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    status = models.CharField(max_length=20, choices=STATUS, default="pending")
+
+    created_at = models.DateTimeField(auto_now_add=True)
