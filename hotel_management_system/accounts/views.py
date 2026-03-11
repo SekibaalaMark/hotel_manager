194518b77a11ac79e3fdeca5349cbc6026f4bcd9
sekibaalamark <markmajje256@gmail.com>
2026-03-11 16:22:02 +0300

    created an API for creating staff user

--- hotel_management_system/accounts/views.py
+++ hotel_management_system/accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import *
+from .permissions import *
+
 
 
 class LoginView(APIView):
@@ -40,6 +42,39 @@ class GuestRegisterView(APIView):
             return Response(
                 {
                     "message": "Guest registered successfully",
+                    "username": user.username
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+
+
+class CreateStaffUserView(APIView):
+
+    permission_classes = [IsManager]
+
+    def post(self, request):
+
+        serializer = StaffCreateSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            user = serializer.save()
+
+            staff_group = Group.objects.get(name="Staff")
+
+            user.groups.add(staff_group)
+
+            return Response(
+                {
+                    "message": "Staff user created successfully",
                     "username": user.username
                 },
                 status=status.HTTP_201_CREATED
