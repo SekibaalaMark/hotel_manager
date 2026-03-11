a0ea23f74da8c41fd88e9ab81dac6c877e95385d
sekibaalamark <markmajje256@gmail.com>
2026-03-11 16:00:24 +0300

    added register api to  accounts urls

--- hotel_management_system/accounts/urls.py
+++ hotel_management_system/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LoginView
+from .views import *
 
 urlpatterns = [
+    path("register/", GuestRegisterView.as_view(), name="guest-register"),
     path("login/", LoginView.as_view(), name="login"),
 ]
