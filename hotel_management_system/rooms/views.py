7100f51ed686f12d42c4ec7e5f259c3e9542a4e9
sekibaalamark <markmajje256@gmail.com>
2026-03-12 14:34:54 +0300

    created a view for creating a room

--- hotel_management_system/rooms/views.py
+++ hotel_management_system/rooms/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Room
+from .serializers import RoomSerializer
+from accounts.permissions import IsManager
+
+
+class CreateRoomView(APIView):
+
+    permission_classes = [IsAuthenticated, IsManager]
+
+    def post(self, request):
+
+        serializer = RoomSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            room = serializer.save()
+
+            return Response(
+                {
+                    "message": "Room created successfully",
+                    "room_number": room.number
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
