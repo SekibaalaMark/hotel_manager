b125c59afcd85f4c4a7d992a3033c9e485105842
sekibaalamark <markmajje256@gmail.com>
2026-03-12 14:46:29 +0300

    created a serializers for creatings rooms in bulk

--- hotel_management_system/rooms/serializers.py
+++ hotel_management_system/rooms/serializers.py
@@ -21,5 +21,37 @@ class RoomSerializer(serializers.ModelSerializer):
 
         return room
 
-        
+
+
+
+class BulkRoomCreateSerializer(serializers.Serializer):
+
+    start_room = serializers.IntegerField()
+    end_room = serializers.IntegerField()
+    room_type = serializers.ChoiceField(choices=Room.ROOM_TYPES)
+    price_per_night = serializers.DecimalField(max_digits=8, decimal_places=2)
+
+    def create(self, validated_data):
+
+        start = validated_data["start_room"]
+        end = validated_data["end_room"]
+        room_type = validated_data["room_type"]
+        price = validated_data["price_per_night"]
+
+        rooms = []
+
+        for number in range(start, end + 1):
+
+            room, created = Room.objects.get_or_create(
+                number=str(number),
+                defaults={
+                    "room_type": room_type,
+                    "price_per_night": price
+                }
+            )
+
+            if created:
+                rooms.append(room)
+
+        return rooms
 
