9d7704ddbe5a229c362aedf3001fefcf8e5c9e4e
sekibaalamark <markmajje256@gmail.com>
2026-03-12 14:50:53 +0300

    added the api view for creating bulk rooms to the rooms urls

--- hotel_management_system/rooms/urls.py
+++ hotel_management_system/rooms/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [  # Must be exactly this name
     path("create/", CreateRoomView.as_view(), name="create-room"),
+    path("bulk-create/", BulkCreateRoomView.as_view()),
 ]
