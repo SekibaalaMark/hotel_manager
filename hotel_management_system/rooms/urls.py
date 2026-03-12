7616bb6911e2f6fab22327e7d6f3a7ef28448af0
sekibaalamark <markmajje256@gmail.com>
2026-03-12 15:00:06 +0300

    added the room info update view in the rooms app urls

--- hotel_management_system/rooms/urls.py
+++ hotel_management_system/rooms/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [  # Must be exactly this name
     path("create/", CreateRoomView.as_view(), name="create-room"),
     path("bulk-create/", BulkCreateRoomView.as_view()),
+    path("update/<int:room_id>/", UpdateRoomView.as_view()),
 ]
