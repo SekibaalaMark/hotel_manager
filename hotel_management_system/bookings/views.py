fc4b1589cfa9136f3b74b5eff04eb9bd5c1a7020
sekibaalamark <markmajje256@gmail.com>
2026-03-17 20:33:16 +0300

    created a booking invoice API view

--- hotel_management_system/bookings/views.py
+++ hotel_management_system/bookings/views.py
@@ -32,3 +32,30 @@ class CreateBookingView(APIView):
             )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+
+
+
+# bookings/views.py
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+
+from .models import Booking
+from .serializers import BookingInvoiceSerializer
+
+
+class BookingInvoiceView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, booking_id):
+
+        booking = get_object_or_404(Booking, id=booking_id)
+
+        serializer = BookingInvoiceSerializer(booking)
+
+        return Response(serializer.data)
