5ca5b8ed88e2322090fe593da6d707fe68baa773
sekibaalamark <markmajje256@gmail.com>
2026-03-17 20:37:48 +0300

    written tests for login serializer

--- hotel_management_system/accounts/tests.py
+++ hotel_management_system/accounts/tests.py
@@ -43,3 +43,41 @@ class CustomUserModelTest(TestCase):
         )
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+        
+        
+        
+        
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+
+        username = data.get("username")
+        password = data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+
+        if not user.is_active and not user.is_superuser:
+            raise serializers.ValidationError("User account is disabled")
+
+        refresh = RefreshToken.for_user(user)
+
+        role = None
+
+        if user.groups.exists():
+            role = user.groups.first().name
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user_id": user.id,
+            "username": user.username,
+            "role": role,
+            "must_change_password": user.must_change_password
+        }
