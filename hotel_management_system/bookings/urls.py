cc72aa9f6acaca3685476344be2bf7391618c4d5
sekibaalamark <markmajje256@gmail.com>
2026-03-17 20:34:02 +0300

    added the booking invoice View to the bookings app urls

--- hotel_management_system/bookings/urls.py
+++ hotel_management_system/bookings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateBookingView
+from .views import *
 
 urlpatterns = [
     path("create/", CreateBookingView.as_view()),
+    path("invoice/<int:booking_id>/", BookingInvoiceView.as_view()),
 ]
