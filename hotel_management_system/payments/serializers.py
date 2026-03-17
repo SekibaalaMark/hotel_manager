f90340f87dd895e59478d00ef388e25c436cdf7f
sekibaalamark <markmajje256@gmail.com>
2026-03-17 20:31:02 +0300

    updated payment serializer to remove manual payment

--- hotel_management_system/payments/serializers.py
+++ hotel_management_system/payments/serializers.py
@@ -3,34 +3,30 @@ from .models import Payment
 from bookings.models import Booking
 
 
+from rest_framework import serializers
+from .models import Payment
+
+
 class PaymentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Payment
         fields = [
             "booking",
-            "amount",
             "payment_method"
         ]
-
-    def validate(self, data):
-
-        booking = data["booking"]
-
-        if booking.status != "pending":
-            raise serializers.ValidationError(
-                "Payment can only be made for pending bookings"
-            )
-
-        return data
 
     def create(self, validated_data):
 
         booking = validated_data["booking"]
 
-        payment = Payment.objects.create(**validated_data)
+        payment = Payment.objects.create(
+            booking=booking,
+            amount=booking.total_cost,  # AUTO amount
+            payment_method=validated_data["payment_method"]
+        )
 
-        # Auto confirm booking after successful payment
+        # Auto confirm booking
         if payment.successful:
             booking.status = "confirmed"
             booking.save()
