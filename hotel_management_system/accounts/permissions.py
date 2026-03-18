05033aefad0d24ce9a292b73ac7669fd011df527
sekibaalamark <markmajje256@gmail.com>
2026-03-18 06:09:56 +0300

    ,created a base permission for a Guest user

--- hotel_management_system/accounts/permissions.py
+++ hotel_management_system/accounts/permissions.py
@@ -4,4 +4,14 @@ class IsManager(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.groups.filter(name="Manager").exists()
-    
+
+
+
+
+from rest_framework.permissions import BasePermission
+
+
+class IsGuest(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="Guest").exists()
