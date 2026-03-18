84416f6823c7bf53312330ca309e5090de65e1c3
sekibaalamark <markmajje256@gmail.com>
2026-03-18 06:03:17 +0300

    created a view for making Bookings by Guests

--- hotel_management_system/bookings/views.py
+++ hotel_management_system/bookings/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import BookingSerializer
+from accounts.permissions import IsGuest
+
+
+class CreateBookingView(APIView):
+
+    permission_classes = [IsAuthenticated, IsGuest]
+
+    def post(self, request):
+
+        serializer = BookingSerializer(
+            data=request.data,
+            context={"request": request}
+        )
+
+        if serializer.is_valid():
+
+            booking = serializer.save()
+
+            return Response(
+                {
+                    "message": "Room booked successfully",
+                    "booking_id": booking.id,
+                    "status": booking.status
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
