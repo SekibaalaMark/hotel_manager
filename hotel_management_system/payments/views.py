42f085a7bca5fe1c7023f3ab795d484ff3046b0b
sekibaalamark <markmajje256@gmail.com>
2026-03-18 06:13:14 +0300

    created a view for making payments

--- hotel_management_system/payments/views.py
+++ hotel_management_system/payments/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import PaymentSerializer
+
+
+class CreatePaymentView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        serializer = PaymentSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            payment = serializer.save()
+
+            return Response(
+                {
+                    "message": "Payment successful",
+                    "booking_status": payment.booking.status
+                },
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
